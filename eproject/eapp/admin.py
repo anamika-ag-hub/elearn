616dfea6bfde1c7984ddcec985be10873d082393
anamika <thisisanamikaag@gmail.com>
2026-01-29 12:56:19 +0530

    Apply .gitignore

--- eproject/eapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import Registration 
-from . models import Addregistration
-
-# Register your models here.
-admin.site.register(Registration)
-admin.site.register(Addregistration)
